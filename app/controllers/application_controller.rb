8b743b6f93f8c794d3e6e605b99f9ef4eaf8887f
Mike Sheridan <=>
2012-12-02 15:03:55 -0600

    updated dashboard, updates some routes so please view your rake routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def layout_by_resource
-  	if devise_controller? && resource_name == :user && action_name == 'new'
+  	if devise_controller? && resource_name == :users && action_name == 'new'
   		"devise"
   	else
   		"application"
